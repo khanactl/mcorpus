000eacc3d495ca0ae03905305978e02a0f79d6c8
jkirton <khanactl@gmail.com>
2018-03-25 10:33:06 -0700

    Nix mcorpus-jwtgen sub-project.  It is not needed.

--- mcorpus-jwtgen/mjwt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# generate a JWT for mcorpus access
-
-# call the mcorpus-jwtgen shaded jar
-java -jar target/mcorpus-jwtgen.jar "$@"
--- mcorpus-jwtgen/src/main/java/com/tll/mcorpus/jwtgen/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * The jwtgen sub-package meant to be built into its own jar
- * whose sole purpose is to generate JWT tokens for use with
- * the mcorpus graphql api.
- *
- * @author jkirton
- */
-package com.tll.mcorpus.jwtgen;
