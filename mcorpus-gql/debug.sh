acb8e8c1fb0264b3800166c70627370ffc17d7f3
jkirton <khanactl@gmail.com>
2018-04-15 15:02:05 -0700

    Exclude log4j2 from shade jar. Add missing maven pom dependency decls.

--- mcorpus-gql/debug.sh
+++ mcorpus-gql/debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd target
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar mcorpus-gql-server.jar -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -cp log4j-api-2.10.0.jar:log4j-core-2.10.0.jar:log4j-slf4j-impl-2.10.0.jar:disruptor-3.3.4.jar -jar mcorpus-gql-server.jar -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector
