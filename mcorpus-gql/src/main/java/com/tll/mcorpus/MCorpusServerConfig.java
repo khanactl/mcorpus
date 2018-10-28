054e5d982e82d64cbb31000917a2dbe4f83058cf
d2d <khanactl@gmail.com>
2018-10-28 11:26:09 -0700

    More concise javadocs for MCorpusServerConfig props.

--- mcorpus-gql/src/main/java/com/tll/mcorpus/MCorpusServerConfig.java
+++ mcorpus-gql/src/main/java/com/tll/mcorpus/MCorpusServerConfig.java
@@ -6,15 +6,38 @@ package com.tll.mcorpus;
  * All needed startup properties shall be explicitly declared herein.
  */
 public class MCorpusServerConfig {
-  // db
+  /**
+   * The fully qualified JDBC data source class name.
+   */
   public String dbDataSourceClassName;
+  /**
+   * The JDBC db connection URL which includes the db username and password.
+   */
   public String dbUrl;
-  // web session
+  
+  /**
+   * The maximum number of web sessions allowed to exist at any instant of time.
+   */
   public int maxWebSessions;
+  /**
+   * The duration of a web session in minutes.
+   */
   public int webSessionDurationInMinutes;
-  // jwt
+  
+  /**
+   * The JWT salt value.
+   */
   public String jwtSalt;
+  /**
+   * The JWT time to live in milli-seconds.
+   */
   public long jwtTtlInMillis;
-  // cookie
+  
+  /**
+   * Flag for whether to send http cookies in the clear (http) or only over https.
+   * <p>
+   * <b>CAUTION</b>: This flag should ALWAYS be <code>true</code> 
+   * in production environments!
+   */
   public boolean cookieSecure;
 }
