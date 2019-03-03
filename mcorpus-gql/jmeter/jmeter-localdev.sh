d6eea55a548b68578c83bdaf54baaaf93057aaea
d2d <khanactl@gmail.com>
2019-03-03 14:52:19 -0800

    Fix load test.

---
+++ mcorpus-gql/jmeter/jmeter-localdev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# archive previous jmeter test run if present
+if [ -d "testoutput" ]; then
+  # prev jmeter test run exists so archive it
+  timestamp=$(date "+%Y-%m-%d_%H:%M:%S")
+  sdir="archive/run-$timestamp"
+  mkdir -p $sdir
+  mv testoutput $sdir
+  mv *.log $sdir
+elif ls *.log > /dev/null 2>&1; then 
+  rm *.log
+fi
+
+# run the jmeter script
+jmeter -n -t MCorpusLoadTest-localdev.jmx -e -o testoutput -l mcorpus-jmeter.log
