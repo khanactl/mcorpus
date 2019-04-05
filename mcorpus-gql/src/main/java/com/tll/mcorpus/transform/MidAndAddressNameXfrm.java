b57418100b55ddfc18f473bfbd66248e971d7b56
d2d <khanactl@gmail.com>
2019-04-05 15:59:20 -0700

    Force file re-name to fix case! (MidAndAddressnameXfrm.java -> MidAndAddressNameXfrm.java).

---
+++ mcorpus-gql/src/main/java/com/tll/mcorpus/transform/MidAndAddressNameXfrm.java
@@ -0,0 +1,28 @@
+package com.tll.mcorpus.transform;
+
+import static com.tll.core.Util.upper;
+import static com.tll.mcorpus.transform.MemberAddressXfrm.addressnameFromString;
+import static com.tll.mcorpus.transform.MemberAddressXfrm.addressnameToString;
+
+import com.tll.mcorpus.dmodel.MidAndAddressname;
+import com.tll.mcorpus.gmodel.MemberAddress.MidAndAddressNameKey;
+
+public class MidAndAddressNameXfrm extends BaseMcorpusTransformer<MidAndAddressNameKey, MidAndAddressname> {
+
+  @Override
+  protected MidAndAddressNameKey fromNonNullBackend(final MidAndAddressname d) {
+    return new MidAndAddressNameKey(
+      d.getMid(),
+      upper(addressnameToString(d.getAddressname()))
+    );
+  }
+
+  @Override
+  protected MidAndAddressname toBackendFromNonNull(final MidAndAddressNameKey g) {
+    return new MidAndAddressname(
+      g.getMid(),
+      addressnameFromString(g.getAddressName()) 
+    );
+  }
+
+}
