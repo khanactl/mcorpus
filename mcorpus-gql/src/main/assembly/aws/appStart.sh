828bf8e2009a7ae3987ad2a8863593d1f9654d73
d2d <khanactl@gmail.com>
2019-04-17 19:48:38 -0700

    Fix aws code deploy scripts.

--- mcorpus-gql/src/main/assembly/aws/appStart.sh
+++ mcorpus-gql/src/main/assembly/aws/appStart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd /home/ec2-user
+mkdir /home/ec2-user/webapp
+cd /home/ec2-user/webapp
 
 dbUrl=$(aws ssm get-parameters --region us-west-2 --names mcorpusDbUrl --with-decryption --query Parameters[0].Value)
 dbUrl=`echo $dbUrl | sed -e 's/^"//' -e 's/"$//'`
@@ -8,7 +9,6 @@ dbUrl=`echo $dbUrl | sed -e 's/^"//' -e 's/"$//'`
 jwtSalt=$(aws ssm get-parameters --region us-west-2 --names jwtSalt --with-decryption --query Parameters[0].Value)
 jwtSalt=`echo $jwtSalt | sed -e 's/^"//' -e 's/"$//'`
 
-# set env vars
 export MCORPUS_DB_DATA_SOURCE_CLASS_NAME=org.postgresql.ds.PGSimpleDataSource
 export MCORPUS_DB_URL=$dbUrl
 export MCORPUS_JWT_SALT=$jwtSalt
@@ -20,5 +20,4 @@ export RATPACK_SERVER__DEVELOPMENT=false
 export RATPACK_SERVER__PORT=5150
 export RATPACK_SERVER__PUBLIC_ADDRESS=https://www.mcorpus-aws.net
 
-# run
 java -Dlog4j.configurationFile=log4j2-aws.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-2.11.1.jar:log4j-core-2.11.1.jar:log4j-slf4j-impl-2.11.1.jar:disruptor-3.4.2.jar -jar mcorpus-gql-server.jar
