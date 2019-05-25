86c3abb05913a449312c24717044c97d8f02b2dc
jp <khanactl@gmail.com>
2019-05-25 14:02:27 -0700

    Dependency version upgrade sweep.

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -29,7 +29,7 @@ export class CdkStack extends cdk.Stack {
       '-Djava.net.preferIPv4Stack=true',
       '-Dlog4j.configurationFile=log4j2-aws.xml',
       '-Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector',
-      '-cp log4j-api-2.11.1.jar:log4j-core-2.11.1.jar:log4j-slf4j-impl-2.11.1.jar:disruptor-3.4.2.jar'
+      '-cp log4j-api-2.11.2.jar:log4j-core-2.11.2.jar:log4j-slf4j-impl-2.11.2.jar:disruptor-3.4.2.jar'
     ].join(' ');
     
     const fargateService = new ecs.LoadBalancedFargateService(this, 'MyFargateService', {
