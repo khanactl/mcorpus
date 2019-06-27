4d784e1e0212ed4898f1b6c151c33c03456f072a
jp <khanactl@gmail.com>
2019-06-27 07:32:51 -0700

    java import cleanup catharsis.

--- mcorpus-gql/src/main/java/com/tll/mcorpus/transform/McuserHistoryXfrm.java
+++ mcorpus-gql/src/main/java/com/tll/mcorpus/transform/McuserHistoryXfrm.java
@@ -2,7 +2,6 @@ package com.tll.mcorpus.transform;
 
 import static com.tll.core.Util.isNull;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
