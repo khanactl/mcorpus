278be351c29e1d1bb9b299be9b9e757ee62e8eab
khana <khanactl@gmail.com>
2019-11-20 08:31:36 -0800

    Remove version callouts in java jar dependencies for app startup.

--- mcorpus-gql/debug.sh
+++ mcorpus-gql/debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd target
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -Dlog4j.configurationFile=log4j2-localdev.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-2.12.1.jar:log4j-core-2.12.1.jar:log4j-slf4j-impl-2.12.1.jar:disruptor-3.4.2.jar -jar mcorpus-gql-server.jar
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -Dlog4j.configurationFile=log4j2-localdev.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api.jar:log4j-core.jar:log4j-slf4j-impl.jar:disruptor.jar -jar mcorpus-gql-server.jar
--- mcorpus-gql/dockstart-local-work.sh
+++ mcorpus-gql/dockstart-local-work.sh
@@ -13,7 +13,7 @@ JAVA_OPTS=(
   '-Djava.net.preferIPv4Stack=true'
   '-Dlog4j.configurationFile=log4j2-aws.xml'
   '-Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector'
-  '-cp log4j-api-2.11.1.jar:log4j-core-2.11.1.jar:log4j-slf4j-impl-2.11.1.jar:disruptor-3.4.2.jar'
+  '-cp log4j-api.jar:log4j-core.jar:log4j-slf4j-impl.jar:disruptor.jar'
 )
 
 docker run \
