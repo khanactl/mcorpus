074034d2a7e6b7563d37558d6deaa877b1bdb8be
khana <khanactl@gmail.com>
2020-01-26 16:30:21 -0800

    dockerbuild.sh Update to comform to new CDK work for a consistent tagging strategy.

--- mcorpus-gql/dockerbuild.sh
+++ mcorpus-gql/dockerbuild.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -e
 
-REPOSITORY_URI='524006177124.dkr.ecr.us-west-2.amazonaws.com/mcorpus-gql'
-MCORPUS_VERSION='${project.version}'
+REPOSITORY_NAME="mcorpus-gql"
+REPOSITORY_URI="524006177124.dkr.ecr.${AWS_DEFAULT_REGION:=us-west-2}.amazonaws.com/$REPOSITORY_NAME"
+MCORPUS_VERSION="${project.version}"
+MCORPUS_BUILD_TIMESTAMP=$(echo "${timestamp}" | sed "s/[^0-9]*//g")
+DOCKER_TAG=${MCORPUS_VERSION}.${MCORPUS_BUILD_TIMESTAMP}
 
 # docker build image
-echo "Building docker image with version tag: $MCORPUS_VERSION.."
-docker build -t $REPOSITORY_URI:$MCORPUS_VERSION -t $REPOSITORY_URI:latest .
+echo "Building docker image with tag: $DOCKER_TAG.."
+docker build -t $REPOSITORY_URI:$DOCKER_TAG .
 
 # docker push to AWS
 echo "Pushing docker image to AWS.."
-$(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
-docker push $REPOSITORY_URI:$MCORPUS_VERSION
-docker push $REPOSITORY_URI:latest
+$(aws ecr get-login --region ${AWS_DEFAULT_REGION:=us-west-2} --no-include-email)
+docker push $REPOSITORY_URI:$DOCKER_TAG
 
 echo "Done."
