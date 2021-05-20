6c6e5217bc7d66bc3522ed693aec4b619e6c62ab
d2d <khanactl@gmail.com>
2021-05-19 21:11:50 -0700

    mcorpus-gql
    dependency updates to latest

--- mcorpus-gql/debug.sh
+++ mcorpus-gql/debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd target
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=log4j2-localdev.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-2.14.0.jar:log4j-core-2.14.0.jar:log4j-slf4j-impl-2.14.0.jar:disruptor-3.4.3.jar -jar mcorpus-gql-server.jar
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=log4j2-localdev.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-2.14.1.jar:log4j-core-2.14.1.jar:log4j-slf4j-impl-2.14.1.jar:disruptor-3.4.4.jar -jar mcorpus-gql-server.jar
--- mcorpus-gql/prod.sh
+++ mcorpus-gql/prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd target
-java -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=log4j2-prod.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-2.14.0.jar:log4j-core-2.14.0.jar:log4j-slf4j-impl-2.14.0.jar:disruptor-3.4.3.jar -jar mcorpus-gql-server.jar
+java -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=log4j2-prod.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-2.14.1.jar:log4j-core-2.14.1.jar:log4j-slf4j-impl-2.14.1.jar:disruptor-3.4.4.jar -jar mcorpus-gql-server.jar
