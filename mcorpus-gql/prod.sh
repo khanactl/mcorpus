c4d54ab6e58ef2e2ecd39109eef258357e7f0a41
d2d <khanactl@gmail.com>
2022-04-02 14:35:34 -0700

    Backend minor version upgrades.
    Version now at 0.9.51-SNAPSHOT.

--- mcorpus-gql/prod.sh
+++ mcorpus-gql/prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd target
-java -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=log4j2-prod.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-2.17.1.jar:log4j-core-2.17.1.jar:log4j-slf4j-impl-2.17.1.jar:disruptor-3.4.4.jar -jar mcorpus-gql-server.jar
+java -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=log4j2-prod.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-2.17.2.jar:log4j-core-2.17.2.jar:log4j-slf4j-impl-2.17.2.jar:disruptor-3.4.4.jar -jar mcorpus-gql-server.jar
