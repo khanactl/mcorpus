7362acffd0452815ff9784cda88f26f4e958329e
jp <khanactl@gmail.com>
2022-08-21 17:58:50 -0700

    log4j2 upgrade to v2.18.0.  mcorpus-gql version now at 0.9.55-SNAPSHOT.

--- mcorpus-gql/debug.sh
+++ mcorpus-gql/debug.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+
+log4j2v="2.18.0"
+disruptorv="3.4.4"
 
 cd target
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=log4j2-localdev.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-2.17.2.jar:log4j-core-2.17.2.jar:log4j-slf4j-impl-2.17.2.jar:disruptor-3.4.4.jar -jar mcorpus-gql-server.jar
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=log4j2-localdev.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-$log4j2v.jar:log4j-core-$log4j2v.jar:log4j-slf4j-impl-$log4j2v.jar:disruptor-$disruptorv.jar -jar mcorpus-gql-server.jar
--- mcorpus-gql/prod.sh
+++ mcorpus-gql/prod.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+
+l4j2v="2.18.0"
+disruptorv="3.4.4"
 
 cd target
-java -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=log4j2-prod.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-2.17.2.jar:log4j-core-2.17.2.jar:log4j-slf4j-impl-2.17.2.jar:disruptor-3.4.4.jar -jar mcorpus-gql-server.jar
+java -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=log4j2-prod.xml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -cp log4j-api-$l4j2v.jar:log4j-core-$l4j2v.jar:log4j-slf4j-impl-$l4j2v.jar:disruptor-$disruptorv.jar -jar mcorpus-gql-server.jar
